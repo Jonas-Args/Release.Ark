54576c9fe6bbbf5e3fa071e9fb20d8bbd98f9345
kaizendevsio <jjeeraldo@gmail.com>
2020-02-14 17:12:52 +0800

    One Click Deploy

--- Ark.FrontEnd.Published/_OneClickDeploy.bat
+++ Ark.FrontEnd.Published/_OneClickDeploy.bat
@@ -1,5 +1,16 @@
 @ECHO OFF
 
+for %%I in (.) do set CurrDirName=%%~nxI
+
+:choice
+set /P c=Deploy %CurrDirName%?[Y/N]
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice
+
+
+:somewhere
+
 echo Building Release.Ark.FrontEnd ...
 echo Copying.. Release.Ark.FrontEnd ...
 
@@ -16,3 +27,13 @@ git push
 
 #echo Deploying to server: Portal
 #plink -pw Ark@123456 arkweb@52.55.94.8 -m oneclick.portal.sh -batch
+
+echo Deployment done. Have a good day :)
+pause
+exit
+
+:somewhere_else
+
+echo Deployment Canceled
+pause
+exit
