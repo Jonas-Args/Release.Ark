09a1ea669fcceb3f0ff59c26c71083b62ebb3fa5
kaizendevsio <jjeeraldo@gmail.com>
2020-02-28 22:33:19 +0800

    Update delivery_info0.blade.php

--- Ark.FrontEnd.Published/resources/views/frontend/delivery_info0.blade.php
+++ Ark.FrontEnd.Published/resources/views/frontend/delivery_info0.blade.php
@@ -5,8 +5,8 @@
     <div id="page-content">
         <section class="slice-xs sct-color-2 border-bottom">
             <div class="container container-sm">
-                <div class="row cols-delimited">
-                    <div class="col-2">
+                <div class="row cols-delimited justify-content-center">
+                    <div class="col-4">
                         <div class="icon-block icon-block--style-1-v5 text-center ">
                             <div class="block-icon mb-0 c-gray-light">
                                 <i class="la la-shopping-cart"></i>
@@ -17,7 +17,7 @@
                         </div>
                     </div>
 
-                    <div class="col-2">
+                    <div class="col-4">
                         <div class="icon-block icon-block--style-1-v5 text-center ">
                             <div class="block-icon mb-0 c-gray-light">
                                 <i class="la la-map-o"></i>
@@ -28,7 +28,7 @@
                         </div>
                     </div>
 
-                    <div class="col-2">
+                    <div class="col-3" style="">
                         <div class="icon-block icon-block--style-1-v5 text-center active">
                             <div class="block-icon mb-0">
                                 <i class="la la-truck"></i>
@@ -39,7 +39,7 @@
                         </div>
                     </div>
 
-                    <div class="col-2">
+                    <div class="col-4">
                         <div class="icon-block icon-block--style-1-v5 text-center">
                             <div class="block-icon mb-0 c-gray-light">
                                 <i class="la la-credit-card"></i>
@@ -59,44 +59,169 @@
                     <div class="col-xl-8">
                         <form class="form-default" data-toggle="validator" action="{{ route('checkout.store_delivery_info') }}" role="form" method="POST">
                             @csrf
+                            @php
+			 $admin_products = array();
+			 $seller_products = array();
+			 foreach (Session::get('cart') as $key => $cartItem){
+				 if(\App\Product::find($cartItem['id'])->added_by == 'admin'){
+					 array_push($admin_products, $cartItem['id']);
+				 }
+				 else{
+					 $product_ids = array();
+					 if(array_key_exists(\App\Product::find($cartItem['id'])->user_id, $seller_products)){
+						 $product_ids = $seller_products[\App\Product::find($cartItem['id'])->user_id];
+					 }
+					 array_push($product_ids, $cartItem['id']);
+					 $seller_products[\App\Product::find($cartItem['id'])->user_id] = $product_ids;
+				 }
+			 }
+             @endphp
+
+                            @if (!empty($admin_products))
                             <div class="card mb-3">
                                 <div class="card-header bg-white py-3">
-                                    <h5 class="heading-6 mb-0">{{__('Delivery type')}}</h5>
+                                    <h5 class="heading-6 mb-0">{{ \App\GeneralSetting::first()->site_name }} Products</h5>
                                 </div>
                                 <div class="card-body">
                                     <div class="row">
-                                        <div class="col-md-10 offset-md-1">
-                                            <div class="row no-gutters">
-                                                <div class="col">
-                                                    <label class="tab-radio w-100 ">
-                                                        <input class="d-none" type="radio" name="shippimg_type" value="home_delivery" checked="checked" onclick="hide_pickup()">
-                                                        <span class="d-block">
-                                                            {{__('Home Delivery')}}
+                                        <div class="col-md-6">
+                                            <table class="table-cart">
+                                                <tbody>
+                                                    @foreach ($admin_products as $key => $id)
+                                                    <tr class="cart-item">
+                                                        <td class="product-image" width="25%">
+                                                            <a href="{{ route('product', \App\Product::find($id)->slug) }}" target="_blank">
+                                                                <img loading="lazy"  src="{{ asset(\App\Product::find($id)->thumbnail_img) }}">
+                                                            </a>
+                                                        </td>
+                                                        <td class="product-name strong-600">
+                                                            <a href="{{ route('product', \App\Product::find($id)->slug) }}" target="_blank" class="d-block c-base-2">
+                                                                {{ \App\Product::find($id)->name }}
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <label class="d-flex align-items-center p-3 border rounded gry-bg c-pointer">
+                                                        <input type="radio" name="shipping_type_admin" value="home_delivery" checked class="d-none" onchange="show_pickup_point(this)" data-target=".pickup_point_id_admin">
+                                                        <span class="radio-box"></span>
+                                                        <span class="d-block ml-2 strong-600">
+                                                            {{ __('Home Delivery') }}
                                                         </span>
                                                     </label>
                                                 </div>
                                                 @if (\App\BusinessSetting::where('type', 'pickup_point')->first()->value == 1)
-                                                    <div class="col">
-                                                        <label class="tab-radio w-100">
-                                                            <input class="d-none" type="radio" name="shippimg_type" id="shippimg_type2" value="pickup_point" onclick="pick_up_points()">
-                                                            <span class="d-block">
-                                                                {{__('Pickup from Pickup Point')}}
+                                                    <div class="col-6">
+                                                        <label class="d-flex align-items-center p-3 border rounded gry-bg c-pointer">
+                                                            <input type="radio" name="shipping_type_admin" value="pickup_point" class="d-none" onchange="show_pickup_point(this)" data-target=".pickup_point_id_admin">
+                                                            <span class="radio-box"></span>
+                                                            <span class="d-block ml-2 strong-600">
+                                                                {{ __('Local Pickup') }}
                                                             </span>
                                                         </label>
                                                     </div>
                                                 @endif
                                             </div>
-                                            <div class="mt-4 row">
-                                                <div class="col-md-6 offset-md-3">
-                                                    <div id="pickup">
-                                                    </div>
+
+                                            @if (\App\BusinessSetting::where('type', 'pickup_point')->first()->value == 1)
+                                                <div class="mt-3 pickup_point_id_admin d-none">
+                                                    <select class="pickup-select form-control-lg w-100" name="pickup_point_id_admin" data-placeholder="Select a pickup point">
+                                                            <option>Select your nearest pickup point</option>
+                                                        @foreach (\App\PickupPoint::where('pick_up_status',1)->get() as $key => $pick_up_point)
+                                                            <option value="{{ $pick_up_point->id }}" data-address="{{ $pick_up_point->address }}" data-phone="{{ $pick_up_point->phone }}">
+                                                                {{ $pick_up_point->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
                                                 </div>
-                                            </div>
+                                            @endif
+
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            @endif
+                            @if (!empty($seller_products))
+                                @foreach ($seller_products as $key => $seller_product)
+                                    <div class="card mb-3">
+                                        <div class="card-header bg-white py-3">
+                                            <h5 class="heading-6 mb-0">{{ \App\Shop::where('user_id', $key)->first()->name }} Products</h5>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="row no-gutters">
+                                                <div class="col-md-6">
+                                                    <table class="table-cart">
+                                                        <tbody>
+                                                            @foreach ($seller_product as $id)
+                                                            <tr class="cart-item">
+                                                                <td class="product-image" width="25%">
+                                                                    <a href="{{ route('product', \App\Product::find($id)->slug) }}" target="_blank">
+                                                                        <img loading="lazy"  src="{{ asset(\App\Product::find($id)->thumbnail_img) }}">
+                                                                    </a>
+                                                                </td>
+                                                                <td class="product-name strong-600">
+                                                                    <a href="{{ route('product', \App\Product::find($id)->slug) }}" target="_blank" class="d-block c-base-2">
+                                                                        {{ \App\Product::find($id)->name }}
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="row">
+                                                        <div class="col-6">
+                                                            <label class="d-flex align-items-center p-3 border rounded gry-bg c-pointer">
+                                                                <input type="radio" name="shipping_type_{{ $key }}" value="home_delivery" checked class="d-none" onchange="show_pickup_point(this)" data-target=".pickup_point_id_{{ $key }}">
+                                                                <span class="radio-box"></span>
+                                                                <span class="d-block ml-2 strong-600">
+                                                                    {{ __('Home Delivery') }}
+                                                                </span>
+                                                            </label>
+                                                        </div>
+                                                        @if (\App\BusinessSetting::where('type', 'pickup_point')->first()->value == 1)
+                                                            @if (is_array(json_decode(\App\Shop::where('user_id', $key)->first()->pick_up_point_id)))
+                                                                <div class="col-6">
+                                                                    <label class="d-flex align-items-center p-3 border rounded gry-bg c-pointer">
+                                                                        <input type="radio" name="shipping_type_{{ $key }}" value="pickup_point" class="d-none" onchange="show_pickup_point(this)" data-target=".pickup_point_id_{{ $key }}">
+                                                                        <span class="radio-box"></span>
+                                                                        <span class="d-block ml-2 strong-600">
+                                                                            {{ __('Local Pickup') }}
+                                                                        </span>
+                                                                    </label>
+                                                                </div>
+                                                            @endif
+                                                        @endif
+                                                    </div>
 
+                                                    @if (\App\BusinessSetting::where('type', 'pickup_point')->first()->value == 1)
+                                                        @if (is_array(json_decode(\App\Shop::where('user_id', $key)->first()->pick_up_point_id)))
+                                                            <div class="mt-3 pickup_point_id_{{ $key }} d-none">
+                                                                <select class="pickup-select form-control-lg w-100" name="pickup_point_id_{{ $key }}" data-placeholder="Select a pickup point">
+                                                                    <option>Select your nearest pickup point</option>
+                                                                    @foreach (json_decode(\App\Shop::where('user_id', $key)->first()->pick_up_point_id) as $pick_up_point)
+                                                                        @if (\App\PickupPoint::find($pick_up_point) != null)
+                                                                            <option value="{{ \App\PickupPoint::find($pick_up_point)->id }}" data-address="{{ \App\PickupPoint::find($pick_up_point)->address }}" data-phone="{{ \App\PickupPoint::find($pick_up_point)->phone }}">
+                                                                                {{ \App\PickupPoint::find($pick_up_point)->name }}
+                                                                            </option>
+                                                                        @endif
+                                                                    @endforeach
+                                                                </select>
+                                                            </div>
+                                                        @endif
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endif
                             <div class="row align-items-center pt-4">
                                 <div class="col-md-6">
                                     <a href="{{ route('home') }}" class="link link--style-3">
@@ -119,22 +244,26 @@
     </div>
 
 @endsection
-{{-- @section('script')
 
-<script type="text/javascript">
+@section('script')
+    <script type="text/javascript">
+        function display_option(key){
 
-    function pick_up_points(){
-        var shippimg_type2 = $('#shippimg_type2').val();
-		$.post('{{ route('shipping_info.get_pick_ip_points') }}',{_token:'{{ csrf_token() }}', shippimg_type2:shippimg_type2}, function(data){
-            $('#pickup').html(data);
-            $('#pickup').show();
-		});
-    }
+        }
+        function show_pickup_point(el) {
+        	var value = $(el).val();
+        	var target = $(el).data('target');
 
-    function hide_pickup(){
-        $('#pickup').hide();
-    }
+            console.log(value);
 
-</script>
+        	if(value == 'home_delivery'){
+                if(!$(target).hasClass('d-none')){
+                    $(target).addClass('d-none');
+                }
+        	}else{
+        		$(target).removeClass('d-none');
+        	}
+        }
 
-@endsection --}}
+    </script>
+@endsection
