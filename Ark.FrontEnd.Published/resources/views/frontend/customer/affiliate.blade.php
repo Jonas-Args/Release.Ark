b0477748aa376cc1e4904c2a76717f478f0bacd1
kaizendevsio <jjeeraldo@gmail.com>
2020-03-05 23:47:09 +0800

    Update affiliate.blade.php

--- Ark.FrontEnd.Published/resources/views/frontend/customer/affiliate.blade.php
+++ Ark.FrontEnd.Published/resources/views/frontend/customer/affiliate.blade.php
@@ -239,7 +239,7 @@
 											@endforeach
 
 											<div class="col-md-4">
-												<img class="dashboard-widget" src="{{asset('uploads/packages/DIRECT_REFERRAL_REWARD.png'}}" onclick="SelectPackage('{{ $businessPackage->id }}');" alt="Alternate Text" style="width:100%" />
+												<img class="dashboard-widget" src="{{asset('uploads/packages/DIRECT_REFERRAL_REWARD.png')}}" onclick="SelectPackage('{{ $businessPackage->id }}');" alt="Alternate Text" style="width:100%" />
 											</div>
 										</div>
 
